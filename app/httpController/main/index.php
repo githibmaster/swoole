2f46c3df979f623f2f4128b912ef226ee9944113
liyanxing <liyanxing@shaoziketang.com>
2019-07-09 15:23:54 +0800

    change index

--- app/httpController/main/index.php
+++ app/httpController/main/index.php
@@ -2,3 +2,14 @@
 
 
 namespace app\httpController\main;
+
+use EasySwoole\Http\AbstractInterface\Controller;
+use EasySwoole\Mysqli\Config;
+use EasySwoole\Mysqli\Mysqli;
+
+class index extends Controller{
+    function index()
+    {
+        // TODO: Implement index() method.
+    }
+}
